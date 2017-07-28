0284f986da104123eabf59c477ee46a3e0a8216b
Nikos Kon <nikoskontothanasis@users.noreply.github.com>
2017-07-28 17:57:51 +0300

    Initial Commit

---
+++ RetroCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RetroCal
+//
+//  Created by Nikos Kontothanasis on 28/07/2017.
+//  Copyright © 2017 Nikos Kontothanasis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
